8c97fac7f43e56dfef21ff691d85a0e0f55f959d
abookish <snsteinca@gmail.com>
2024-01-28 20:26:33 -0800

    making methods more usable, export

--- fetchAPIMethods.js
+++ fetchAPIMethods.js
@@ -1,27 +1,25 @@
-import URI from 'urijs';
+import URI from 'uri-js';
 
-// /records endpoint
-window.path = 'http://localhost:3000';
+// defines a uri string arg of an object with atttribute value pairs
+export function getURI(recordsEndPoint, optionsObject) {
+  const defaultLimit = 10;
+  const uri = new URI(recordsEndPoint);
 
-
- // defines a uri string arg of an object with atttribute value pairs
-const getURI = (optionsObject) => {
- const defaultNumItems = 10;
-  const uri = new URI(window.path);
-  uri.addQuery('limit', defaultNumItems.toString());
-//iterate through any entries to query string for attribute value
-  if (Object.entries(optionsObject.length > 0)) {
-    for (const [attributeName, expectedValue] of Object.entries(optionsObject)){
+  const limit = optionsObject?.limit ? optionsObject?.limit : defaultLimit;
+  uri.addQuery('limit', limit.toString());
+  // iterate through any entries to query string for attribute value
+  if (Object.entries(optionsObject?.length > 0)) {
+    for (const [attributeName, expectedValue] of Object.entries(optionsObject)) {
       uri.addQuery(attributeName.toString(), expectedValue.toString());
+    }
   }
-}
   return uri;
-};
+}
 
 // eslint-disable-next-line consistent-return
-async function fetchDataJson(optionsObject) {
+export async function fetchDataJson(recordsEndPoint, optionsObject = null) {
   try {
-    const response = await fetch(getURI(optionsObject));
+    const response = await fetch(getURI(recordsEndPoint, optionsObject));
     if (!response.ok) {
       console.log(`error in fetch with ${response.statusText}`);
       throw new Error(response);
@@ -32,11 +30,11 @@ async function fetchDataJson(optionsObject) {
   }
 }
 
-async function fetchResultIsValid(optionsObject) {
-  const fetchPayload = await fetchDataJson(optionsObject);
+export async function fetchResultIsValid(recordsEndPoint, optionsObject) {
+  const fetchPayload = await fetchDataJson(recordsEndPoint, optionsObject);
   return fetchPayload?.length > 0;
 }
-async function cloneObjectWithNewAttribute(optionsObject, attributeName, attributeValue) {
+export async function cloneObjectWithNewAttribute(optionsObject, attributeName, attributeValue) {
   if (!optionsObject) { // if nothing passed in, handle by making new obj
     const newObj = {};
     newObj[attributeName] = attributeValue;
@@ -46,40 +44,40 @@ async function cloneObjectWithNewAttribute(optionsObject, attributeName, attribu
   clone.page = newPageValue;
   return clone;
 }
-async function getAlternateOptionsAttributesWithValidData(optionsObject, newAttributesObject) {
-  //runs alternate fetch on a copy of object with a changed value of the passed in attribute, or adding attribute/value 
-  //if fetch payload includes results, store attribute and value pair
+export async function getAlternateOptionsAttributesWithValidData(optionsObject, newAttributesObject) {
+  // runs alternate fetch on a copy of object with a changed value of the passed in attribute, or adding attribute/value
+  // if fetch payload includes results, store attribute and value pair
   const attributeValueWithResults = {
-  
+
   };
   async function setAttributesForValidResults(attributeName, attributeValue) {
     const optionsWithNewAttribute = await cloneObjectWithNewAttribute(optionsObject, attributeValue);
     if (await fetchResultIsValid(optionsWithNewAttribute)) { attributesWithResults[attributeName] = attributeValue; }
   }
-  if(Object.entries(newAttributesObject)?.length > 0) {
-    for (const [attributeName, expectedValue] of Object.entries(optionsObject)){
-      await setAttributesForValidResults([attributeName, expectedValue])
+  if (Object.entries(newAttributesObject)?.length > 0) {
+    for (const [attributeName, expectedValue] of Object.entries(optionsObject)) {
+      await setAttributesForValidResults([attributeName, expectedValue]);
+    }
   }
-}
 
   return attributeValueWithResults;
 }
-// filterOptionsArray expects array of objs with keys: attribute, filter characteristic 
-async function getDataAttributes(fetchPayloadJson, filterOptionsArray) {
+// filterOptionsArray expects array of objs with keys: attribute, filter characteristic
+export async function getDataAttributes(fetchPayloadJson, filterOptionsArray) {
   const dataAttributes = {
-    
+
   };
-  if (fetchPayloadJson?.length > 0 && filterOptionsArray?.length >0) {
+  if (fetchPayloadJson?.length > 0 && filterOptionsArray?.length > 0) {
     for (const filterOptionsObject of filterOptionsArray) {
-      if (Object.entries(filterOptionsObject).length >0) {
+      if (Object.entries(filterOptionsObject).length > 0) {
         for (const [attributeName, expectedValue] of Object.entries(filterOptionsObject)) {
-          const dataAttributesKey = `${attributeName}${expectedValue.toString()}`
-          dataAttributes[dataAttributesKey] =  await fetchPayloadJson?.filter((eachObj) => eachObj[attributeName] === expectedValue)
+          const dataAttributesKey = `${attributeName}${expectedValue.toString()}`;
+          dataAttributes[dataAttributesKey] = await fetchPayloadJson?.filter((eachObj) => eachObj[attributeName] === expectedValue);
         }
       }
     }
-    };
-    
+  }
+
   return dataAttributes;
 }
 
