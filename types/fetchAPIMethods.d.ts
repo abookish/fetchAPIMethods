152de50c92b83e4781c31460b44e21d55a69fda3
abookish <snsteinca@gmail.com>
2024-01-30 09:26:52 -0800

    declaring module itself

--- types/fetchAPIMethods.d.ts
+++ types/fetchAPIMethods.d.ts
@@ -1,16 +1,18 @@
 
+declare module "fetchapimethods" {
 // defines a uri string arg of an object with atttribute value pairs
-declare function getURI(recordsEndPoint: string, optionsObject: Object | undefined): string
+function getURI(recordsEndPoint: string, optionsObject: Object | undefined): string
 
 
 interface apiResponse {
   data: any
 }
 // eslint-disable-next-line consistent-return
-declare function fetchDataJson(recordsEndPoint:string, optionsObject: Object |undefined): Promise<apiResponse | undefined>
+function fetchDataJson(recordsEndPoint:string, optionsObject: Object |undefined): Promise<apiResponse | undefined>
 
-declare function fetchResultIsValid(recordsEndPoint: string, optionsObject: Object | undefined): Promise<boolean>
-declare function cloneObjectAddAttribute(optionsObject: Object | undefined, attributeName: String, attributeValue: any): Promise<Object>
+function fetchResultIsValid(recordsEndPoint: string, optionsObject: Object | undefined): Promise<boolean>
+function cloneObjectAddAttribute(optionsObject: Object | undefined, attributeName: String, attributeValue: any): Promise<Object>
 
 // filterOptionsArray expects array of objs with keys: attribute, filter characteristic
-declare function getDataAttributes(fetchPayloadJson: Promise<apiResponse | undefined>, filterOptionsArray: Object[] | undefined): Promise<Object>
+function getDataAttributes(fetchPayloadJson: Promise<apiResponse | undefined>, filterOptionsArray: Object[] | undefined): Promise<Object>
+}
